362f5e6c179b076707f72f406c535ad9b6aca156
曹博轩 <1005854175@qq.com>
2026-05-30 03:44:50 +0800

    Refine project overview for DICT scenarios

--- src/main/java/com/xianggon/trafficops/config/OpenApiConfig.java
+++ src/main/java/com/xianggon/trafficops/config/OpenApiConfig.java
@@ -1,7 +1,6 @@
 package com.xianggon.trafficops.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
@@ -17,10 +16,10 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("交通信息化工单与设备台账系统 API")
                         .version("v1.0")
-                        .description("面向高速路段设备台账、巡检记录、故障工单和运维统计的 Spring Boot 示例项目")
-                        .contact(new Contact()
-                                .name("xiangchixiong-code")
-                                .url("https://github.com/xiangchixiong-code")))
+                        .description("""
+                                面向运营商政企/DICT行业信息化场景的后端接口服务。
+                                业务范围包括高速路段设备台账、巡检记录、故障工单、操作日志、统计看板和 Excel 台账导入导出。
+                                """))
                 .servers(List.of(new Server()
                         .url("http://localhost:8080")
                         .description("本地开发环境")));
